26498bd96ae9a9da530d361f880237797c9f4df3
Taylor D. Brown <taybrown@umich.edu>
2013-10-03 20:21:17 -0700

    Create XML_Checker.py

---
+++ XML_Checker.py
@@ -0,0 +1,22 @@
+#	XML documents must have a root element
+#	XML elements must have a closing tag
+#	XML tags are case sensitive
+#	XML elements must be properly nested
+#	XML attribute values must be quoted
+
+class XML_Checker:
+  
+  def xml_check_attrib():
+    pass
+    
+  def xml_check_nest():
+    pass
+    
+  def xml_check_case():
+    pass
+    
+  def xml_root_check():
+	  pass
+	  
+  def xml_close_check():
+    pass
