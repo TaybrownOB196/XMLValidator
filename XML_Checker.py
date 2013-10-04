5c3972930613169457c94d2dad71f98d7f6ee48c
Taylor D. Brown <taybrown@umich.edu>
2013-10-03 21:33:12 -0700

    Update XML_Checker.py

--- XML_Checker.py
+++ XML_Checker.py
@@ -5,9 +5,24 @@
 #	XML attribute values must be quoted
 
 class XML_Checker:
+  """ This class contains all the static methods necessary to check the xml file"""
+  
+ 
+  current_tag = ""
+  #This variable will be used as a dictionary to store the element names
+  tag_list = {};
+  
+  #This static method checks the filename to see if it ends with a ".xml" exstension
+  #If True, then the file is supposed to be an XML file
+  #If False, then either the file's extension was not added or the file is not an XML file
+  def xml_file_check(filename):
+  	if filename[:-4] != ".xml":
+  		return False
+  	else
+  		return True
   
   def xml_check_attrib():
-    pass
+	pass
     
   def xml_check_nest():
     pass
@@ -16,7 +31,7 @@ class XML_Checker:
     pass
     
   def xml_root_check():
-	  pass
+	pass
 	  
   def xml_close_check():
     pass
