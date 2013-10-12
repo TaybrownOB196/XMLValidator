754c016fe7a2f0599ed2a6f30fd460b66c9a011b
Taylor D. Brown <taybrown@umich.edu>
2013-10-12 11:50:55 -0700

    Update XML_Checker.py

--- XML_Checker.py
+++ XML_Checker.py
@@ -1,3 +1,5 @@
+#   XML documents must have the version number 
+#    and the language encoding on the first line
 #	XML documents must have a root element
 #	XML elements must have a closing tag
 #	XML tags are case sensitive
@@ -5,7 +7,7 @@
 #	XML attribute values must be quoted
 
 class XML_Checker(object):
-	""" This class contains all the static methods necessary to check the xml file"""
+	""" This class contains all the static methods necessary to check an XML file's validation"""
 
   	def __init__(self, filename):
   		if __xml_file_check(filename):
@@ -15,6 +17,10 @@ class XML_Checker(object):
   			self.tag_list = {}
   			#This variable will store the string name of the current tag that the file stream is on
   			self.current_tag = ""
+  			#
+  			self.version = ""
+  			#
+  			self.declaration = ""
   			
   	#This static method checks the filename to see if it ends with a ".xml" exstension
   	#If True, then the file is supposed to be an XML file
@@ -31,9 +37,22 @@ class XML_Checker(object):
 	def __fill_tag_list(tag_name, level):
  		self.tag_list[tag_name] = level
  	
- 	def check_for_encoding():
- 		pass
- 	
+ 	#This method will be used to check to make sure that the first line in the XML file is
+ 	#the version number and language encoding.
+ 	def check_for_declaration():
+ 		self.xml_file.seek(0)
+ 		firstline = self.xml_file.readline()
+ 		if firstline[:2] != "<?":
+ 			return False
+ 		num = firstline.find("?>", 0, len(firstline))
+ 		if num == -1:
+ 			return False
+ 		self.declaration = firstline[:num+2]
+ 		return True
+ 		
+ 	def get_version():
+ 		self.declaration.find("version", 2, len(self.declaration)
+ 		
 	def xml_check_attrib():
 		pass
     
@@ -47,4 +66,4 @@ class XML_Checker(object):
 		pass
 	  
 	def xml_close_check():
-    	pass
+    	self.xml_file.close()
