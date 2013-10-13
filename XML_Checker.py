ff3fd3ee164da2603b673fdcd1c510025d4127be
Taylor D. Brown <taybrown@umich.edu>
2013-10-13 09:26:26 -0700

    Update XML_Checker.py

--- XML_Checker.py
+++ XML_Checker.py
@@ -1,5 +1,5 @@
-#   XML documents must have the version number 
-#    and the language encoding on the first line
+#	XML documents must have the version number 
+#	 and the language encoding on the first line
 #	XML documents must have a root element
 #	XML elements must have a closing tag
 #	XML tags are case sensitive
@@ -7,24 +7,24 @@
 #	XML attribute values must be quoted
 
 class XML_Checker(object):
-	""" This class contains all the static methods necessary to check an XML file's validation"""
+	""" This class contains all the static methods necessary to check an XML file's validation."""
 
   	def __init__(self, filename):
   		if __xml_file_check(filename):
-  			#This variable will hold the object that reads through the XML file
+  			#This variable will hold the object that reads through the XML file.
   			self.xml_file = open(filename, "r")
-  			#This variable will be used as a dictionary to store the element names
+  			#This variable will be used as a dictionary to store the element names.
   			self.tag_list = {}
-  			#This variable will store the string name of the current tag that the file stream is on
+  			#This variable will store the string name of the current tag that the file stream is on.
   			self.current_tag = ""
   			#
   			self.version = ""
   			#
   			self.declaration = ""
   			
-  	#This static method checks the filename to see if it ends with a ".xml" exstension
-  	#If True, then the file is supposed to be an XML file
-  	#If False, then either the file's extension was not added or the file is not an XML file
+  	#This static method checks the filename to see if it ends with a ".xml" exstension.
+  	#If True, then the file is supposed to be an XML file.
+  	#If False, then either the file's extension was not added or the file is not an XML file.
 	def __xml_file_check(filename):
 		if filename[-4:] != ".xml":
   			return False
@@ -33,7 +33,7 @@ class XML_Checker(object):
   
   	#This method is used to fill the tag list with an element tag from the XML that we are
   	#currently looking at. The tag list will be a ditionary that holds the name of the 
-  	#element and its nest level in the XML file
+  	#element and its nest level in the XML file.
 	def __fill_tag_list(tag_name, level):
  		self.tag_list[tag_name] = level
  	
@@ -51,19 +51,19 @@ class XML_Checker(object):
  		return True
  		
  	def get_version():
- 		self.declaration.find("version", 2, len(self.declaration)
+ 		test = self.declaration.find("\"", 2, len(self.declaration)
+ 		if test == -1:
+ 			return False
+ 		if self.declaration[test+1:test+3] != "1.":
+ 			return False
+ 		return self.declaration[test+1:self.declaration.find("\"", test+3, len(self.declaration))]
  		
 	def xml_check_attrib():
 		pass
     
 	def xml_check_nest():
-    	pass
-    
-	def xml_check_case():
-    	pass
-    
-	def xml_root_check():
-		pass
-	  
+    	self.xml_file.seek(len(self.declaration)+1)
+    	
+	
 	def xml_close_check():
     	self.xml_file.close()
